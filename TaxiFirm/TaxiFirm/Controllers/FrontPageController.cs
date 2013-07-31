ec925638c2940cd634f20e35a5fe1ae09a8acbb1
752050943 <752050943@qq.com>
2013-08-01 06:05:10 +0800

    kl

--- TaxiFirm/TaxiFirm/Controllers/FrontPageController.cs
+++ TaxiFirm/TaxiFirm/Controllers/FrontPageController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TaxiFirm.Models.Customer;
+using TaxiFirm.Models.News;
+using TaxiFirm.Models;
 namespace TaxiFirm.Controllers
 {
     public class FrontPageController : Controller
     {
         //
         // GET: /FrontPage/
+        DataClasses1DataContext db = new DataClasses1DataContext();
         [HttpPost]
         public ActionResult GetCredit(string invoiceNumber)
         {
@@ -46,6 +49,66 @@ namespace TaxiFirm.Controllers
         }
         public ActionResult News()
         {
+            string type = Request.QueryString.Get("type");
+            MyPage page = new MyPage();
+            if (type!=null)
+            {
+                if (type.Equals("search"))   //搜索类型
+                {
+                    int page1 = int.Parse(Request.QueryString.Get("page"));
+                    string NameID = Request.QueryString.Get("NameID");
+                    try
+                    {
+                        int id = int.Parse(NameID);
+                        News news = new NewsHandle().getNewsById(id);
+                        List<News> newses = new List<News>();
+                        if (news.Title != null && !news.Equals(""))
+                        {
+                            newses.Add(news);
+                        }
+                        ViewData["type"] = "search";
+                        ViewData["newses"] = newses;
+
+                        page.CurrentPage = page1;
+                        page.CountPerPage = 10;
+                        page.WholePage = 1;
+                        ViewData["page"] = page;
+                        ViewData["NameID"] = NameID;
+                    }
+                    catch
+                    {
+                        page.CurrentPage = page1;
+                        List<News> newses = new NewsHandle().GetNewsByNameByPage(page, NameID);
+                        ViewData["type"] = "search";
+                        ViewData["newses"] = newses;
+                        ViewData["page"] = page;
+                        ViewData["NameID"] = NameID;
+                    }
+                }
+            }
+            else
+            {
+                int page1;
+                if (Request.QueryString.Get("page")==null)
+                {
+                    page1 = 1;
+                    page.CurrentPage = page1;
+                    List<News> newses = new NewsHandle().GetNewsByPage(page);
+                    ViewData["type"] = "common";
+                    ViewData["newses"] = newses;
+                    ViewData["page"] = page;
+                }
+                else
+                {
+                    page1 = int.Parse(Request.QueryString.Get("page"));
+                    page.CurrentPage = page1;
+                    List<News> newses = new NewsHandle().GetNewsByPage(page);
+                    ViewData["type"] = "common";
+                    ViewData["newses"] = newses;
+                    ViewData["page"] = page;
+                }
+
+            }
             return View();
         }
         public ActionResult Notification()
@@ -71,6 +134,11 @@ namespace TaxiFirm.Controllers
         }
         public ActionResult NewsContent()
         {
+            int news_id = int.Parse(Request.QueryString.Get("NWID"));
+            News news = new News();
+            NewsHandle handler = new NewsHandle();
+            news = handler.getNewsById(news_id);
+            ViewData["news"] = news;
             return View();
         }
         public ActionResult Contact()
